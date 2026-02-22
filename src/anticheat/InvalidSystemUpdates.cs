60c46dcbf55c21dd9ca91e41782cb3d2df1712eb
Mr Diamond <49098391+MrDiamond64@users.noreply.github.com>
2026-02-22 16:56:26 -0700

    feat: Anticheat now detects invalid SwitchSystem updates

--- src/anticheat/InvalidSystemUpdates.cs
+++ src/anticheat/InvalidSystemUpdates.cs
@@ -19,6 +19,38 @@ namespace HydraMenu.anticheat
 				blockRpc = true;
 				return;
 			}
+
+			switch(system)
+			{
+				case SystemTypes.Electrical:
+					ValidateSwitchSystem(player, reader, ref blockRpc);
+					break;
+			}
+		}
+
+		public static void ValidateSwitchSystem(PlayerControl player, MessageReader reader, ref bool blockRpc)
+		{
+			byte switches = reader.ReadByte();
+
+			if(switches.HasBit(128))
+			{
+				Hydra.notifications.Send("Anticheat", $"{player.Data.PlayerName} attempted to update switch system with 8th bit active ({switches}).");
+				Anticheat.Punish(player);
+				blockRpc = true;
+			}
+			else if(switches > 5)
+			{
+				Hydra.notifications.Send("Anticheat", $"{player.Data.PlayerName} attempted to toggle an invalid switch: {switches}.");
+				Anticheat.Punish(player);
+				blockRpc = true;
+			}
+
+			if(player.Data.IsDead)
+			{
+				Hydra.notifications.Send("Anticheat", $"{player.Data.PlayerName} attempted toggle light switches while dead.");
+				Anticheat.Punish(player);
+				blockRpc = true;
+			}
 		}
 	}
 }
