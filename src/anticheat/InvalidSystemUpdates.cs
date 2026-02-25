c66641a02400b18510e4539bee5e598ff7728a98
Mr Diamond <49098391+MrDiamond64@users.noreply.github.com>
2026-02-24 19:13:07 -0700

    feat: Anticheat has more strict checks on updating systems when dead

--- src/anticheat/InvalidSystemUpdates.cs
+++ src/anticheat/InvalidSystemUpdates.cs
@@ -1,10 +1,14 @@
 ﻿using Hazel;
 using InnerNet;
+using System.Linq;
 
 namespace HydraMenu.anticheat
 {
 	internal class InvalidSystemUpdate : ICheck
 	{
+		// TODO: Maybe change the variable name to something shorter lol?
+		private static readonly SystemTypes[] SystemsThatCanBeUpdatedWhenDead = { SystemTypes.MedBay, SystemTypes.Sabotage };
+
 		public static void OnSystemUpdate(MessageReader reader, ref bool blockRpc)
 		{
 			if(!Anticheat.Enabled || !Anticheat.CheckInvalidSystemUpdates) return;
@@ -15,6 +19,14 @@ namespace HydraMenu.anticheat
 			if(!ShipStatus.Instance.Systems.ContainsKey(system))
 			{
 				Hydra.notifications.Send("Anticheat", $"{player.Data.PlayerName} tried to update system {system} when the current map has no such system.");
+				Anticheat.Punish(player);
+				blockRpc = true;
+				return;
+			}
+
+			if(player.Data.IsDead && !SystemsThatCanBeUpdatedWhenDead.Contains(system))
+			{
+				Hydra.notifications.Send("Anticheat", $"{player.Data.PlayerName} tried to update system {system} while dead.");
 				Anticheat.Punish(player);
 				blockRpc = true;
 				return;
@@ -34,20 +46,13 @@ namespace HydraMenu.anticheat
 
 			if(switches.HasBit(128))
 			{
-				Hydra.notifications.Send("Anticheat", $"{player.Data.PlayerName} attempted to update switch system with 8th bit active ({switches}).");
+				Hydra.notifications.Send("Anticheat", $"{player.Data.PlayerName} attempted to bulk-update switches: {switches}.");
 				Anticheat.Punish(player);
 				blockRpc = true;
 			}
 			else if(switches > 5)
 			{
 				Hydra.notifications.Send("Anticheat", $"{player.Data.PlayerName} attempted to toggle an invalid switch: {switches}.");
-				Anticheat.Punish(player);
-				blockRpc = true;
-			}
-
-			if(player.Data.IsDead)
-			{
-				Hydra.notifications.Send("Anticheat", $"{player.Data.PlayerName} attempted toggle light switches while dead.");
 				Anticheat.Punish(player);
 				blockRpc = true;
 			}
