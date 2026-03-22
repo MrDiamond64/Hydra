3388008d2777694cc7eec000d900ef448c19f1cf
Mr Diamond <49098391+MrDiamond64@users.noreply.github.com>
2026-03-22 14:03:17 -0700

    feat: Add Disable Vents feature to auto-kick players out from vents

--- src/features/Troll.cs
+++ src/features/Troll.cs
@@ -23,42 +23,30 @@ namespace HydraMenu.features
 			}
 		}
 
-		/*
-		This feature currently does not work. When a player enters a vent, the game runs the PlayerPhysics::CoEnterVent function which is an IEnumerator
-		This IEnumerator function makes the player play the vent walk animation, enter vent animation, and update VentilationSystem
-		which is used to let the host of the lobby know that someone is in a vent for the vent cleaning feature
-		
-		We have to wait for the end of the IEnumerator state to make sure the player who vented sends the VentilationSystem update, so we can clean the vent and get them booted from the vent
-		Only problem is that Harmony's postfix function runs after the function call finishes, not after the IEnumerator completes.
-		This means the player has not yet notificed the host of the ventilationsystem update and so cleaning the vent wont kick them out
-
-		I'm not knowlegable on IEnumerators, so I am unable to get this feature to work. The closest I've gotten is to patch the Vent::EnterVent
-		function which is called by PlayerPhysics::CoEnterVent, but that is still too early
-
-		[HarmonyPatch(typeof(Vent), nameof(Vent.EnterVent))]
+		[HarmonyPatch(typeof(VentilationSystem), nameof(VentilationSystem.Deserialize))]
 		public static class BlockVenting
 		{
 			public static bool Enabled { get; set; } = false;
 
-			static void Postfix(Vent __instance, PlayerControl pc)
+			static void Postfix(VentilationSystem __instance)
 			{
 				if(!Enabled) return;
 
-				Hydra.Log.LogInfo($"Detected someone venting in vent {__instance.Id}");
+				Hydra.Log.LogInfo($"Received updated for VentilationSystem, going to kick out all players who are inside a vent");
 
-				if(AmongUsClient.Instance.AmHost)
+				if(__instance.PlayersInsideVents.Count >= PlayerControl.AllPlayerControls.Count)
 				{
-					Hydra.Log.LogInfo("We're the host, we can just use the BootFromVent RPC.");
-					// VentilationSystem::BootImpostersFromVent sends the BootFromVent RPC using the player who should be booted's instance of PlayerPhysics
-					// so we replicate that here
-					pc.MyPhysics.RpcBootFromVent(__instance.Id);
-				} else
+					Hydra.Log.LogInfo($"Apparently there are more people inside of vents than people inside the game, the host may be trying to overload our game! Players in vents: {__instance.PlayersInsideVents.Count}, total players: {PlayerControl.AllPlayerControls.Count}"); return;
+				}
+
+				foreach(byte ventId in __instance.PlayersInsideVents.Values)
 				{
-					Hydra.Log.LogInfo("We're not host, so we are using vent clean method.");
-					VentilationSystem.Update(VentilationSystem.Operation.StartCleaning, __instance.Id);
+					if(ventId >= ShipStatus.Instance.AllVents.Count) continue;
+
+					Hydra.Log.LogInfo($"Kicked someone out of vent {ventId}");
+					VentilationSystem.Update(VentilationSystem.Operation.StartCleaning, ventId);
 				}
 			}
 		}
-		*/
 	}
 }
