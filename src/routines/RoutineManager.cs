99fece2fb9309dd46e85c8ad8cc4dd3f47a4b1c0
dr.hacker3 <imegg124@gmail.com>
2026-08-11 21:53:22 +0300

    Update RoutineManager.cs

--- src/routines/RoutineManager.cs
+++ src/routines/RoutineManager.cs
@@ -1,50 +1,51 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using UnityEngine;
 
 namespace HydraMenu.routines
 {
-	internal class RoutineManager : MonoBehaviour
-	{
-		public AutoTriggerSporesRoutine autoTriggerSpores = new AutoTriggerSporesRoutine();
-		public DiscoHostRoutine discoHost = new DiscoHostRoutine();
-		public DoorTrollerRoutine doorTroller = new DoorTrollerRoutine();
-		public JailPlayerRoutine jailPlayer = new JailPlayerRoutine();
-		public PetPlayerRoutine petPlayer = new PetPlayerRoutine();
-		public PlayerFollowerRoutine playerFollower = new PlayerFollowerRoutine();
-		public ReportBodySpam reportBodySpam = new ReportBodySpam();
-		public TeleportSpammer teleportSpammer = new TeleportSpammer();
+    internal class RoutineManager : MonoBehaviour
+    {
+        public AutoTriggerSporesRoutine autoTriggerSpores = new AutoTriggerSporesRoutine();
+        public DiscoHostRoutine discoHost = new DiscoHostRoutine();
+        public DoorTrollerRoutine doorTroller = new DoorTrollerRoutine();
+        public JailPlayerRoutine jailPlayer = new JailPlayerRoutine();
+        public PetPlayerRoutine petPlayer = new PetPlayerRoutine();
+        public PlayerFollowerRoutine playerFollower = new PlayerFollowerRoutine();
+        public ReportBodySpam reportBodySpam = new ReportBodySpam();
+        public TeleportSpammer teleportSpammer = new TeleportSpammer();
+        public AutoKickAll autoKickAll = new AutoKickAll();
 
-		public readonly IRoutine[] routineList;
+        public readonly IRoutine[] routineList;
 
-		public RoutineManager()
-		{
-			routineList = [ autoTriggerSpores, discoHost, doorTroller, jailPlayer, petPlayer, playerFollower, reportBodySpam, teleportSpammer ];
-		}
+        public RoutineManager()
+        {
+            routineList = [autoTriggerSpores, discoHost, doorTroller, jailPlayer, petPlayer, playerFollower, reportBodySpam, teleportSpammer, autoKickAll];
+        }
 
-		public void Update()
-		{
-			foreach(IRoutine routine in routineList)
-			{
-				if(!routine.Enabled) continue;
+        public void Update()
+        {
+            foreach (IRoutine routine in routineList)
+            {
+                if (!routine.Enabled) continue;
 
-				routine.Run();
-			}
-		}
+                routine.Run();
+            }
+        }
 
-		[HarmonyPatch(typeof(GameData), nameof(GameData.OnDisconnected))]
-		class DisconnectHandler
-		{
-			static void Prefix()
-			{
-				Hydra.Log.LogInfo("Player disconnected from the lobby, disabling relevant routines");
+        [HarmonyPatch(typeof(GameData), nameof(GameData.OnDisconnected))]
+        class DisconnectHandler
+        {
+            static void Prefix()
+            {
+                Hydra.Log.LogInfo("Player disconnected from the lobby, disabling relevant routines");
 
-				foreach(IRoutine routine in Hydra.routines.routineList)
-				{
-					if(!routine.Enabled) continue;
+                foreach (IRoutine routine in Hydra.routines.routineList)
+                {
+                    if (!routine.Enabled) continue;
 
-					routine.OnDisconnect();
-				}
-			}
-		}
-	}
+                    routine.OnDisconnect();
+                }
+            }
+        }
+    }
 }
