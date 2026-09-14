66cdb9c9873e0c6c76a4728bc4a2606badff6618
𝓡𝚎𝚊𝚌𝚝𝚘r𝓒𝚘𝚛e𝓓𝚎v <165254059+ReactorCoreDev@users.noreply.github.com>
2026-09-14 13:01:09 +0200

    feat: unsupported warning fixes

--- src/ui/MainUI.cs
+++ src/ui/MainUI.cs
@@ -1,6 +1,7 @@
-﻿using HydraMenu.ui.sections;
+using HydraMenu.ui.sections;
 using System;
 using UnityEngine;
+using Il2CppInterop.Runtime.Attributes;
 
 namespace HydraMenu.ui
 {
@@ -168,7 +169,8 @@ namespace HydraMenu.ui
 				mousePos.y >= windowPosition.y &&
 				mousePos.y <= (windowPosition.y + WindowSize.y);
 		}
-
+		
+		[HideFromIl2Cpp]
 		private void RenderTab(byte position, Section section)
 		{
 			Rect rect = new Rect(
@@ -193,7 +195,8 @@ namespace HydraMenu.ui
 			public float UiScale { get; set; }
 			public bool DisableNotifications { get; set; }
 		}
-
+		
+		[HideFromIl2Cpp]
 		public MainUIConfig GetConfigData()
 		{
 			return new MainUIConfig
@@ -205,7 +208,8 @@ namespace HydraMenu.ui
 				DisableNotifications = Hydra.notifications.disableNotifications
 			};
 		}
-
+		
+		[HideFromIl2Cpp]
 		public void LoadConfigData(MainUIConfig configData)
 		{
 			if(configData == null) return;
